9da570796a3945bbcabc523d347728088f988dab
Koji Sato <utautaouka@gmail.com>
2024-01-13 14:37:17 +0900

    logout処理の修正

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -33,10 +33,10 @@ class AuthController extends Controller
 
     public function logout(Request $request): JsonResponse
     {
-        Auth::logout();
+        Auth::guard('web')->logout();
         $request->session()->invalidate();
         $request->session()->regenerateToken();
 
-        return response()->json(true);
+        return response()->json(['message' => 'ログアウトしました']);
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -17,7 +17,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::post('/login', [AuthController::class, 'login']);
-Route::post('/logout', [AuthController::class, 'logout']);
 
 // Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 //     return $request->user();
@@ -30,5 +29,6 @@ Route::group(
         });
         Route::get('/word', [WordController::class, 'index']);
         Route::post('/word', [WordController::class, 'store']);
+        Route::post('/logout', [AuthController::class, 'logout']);
     }
 );
